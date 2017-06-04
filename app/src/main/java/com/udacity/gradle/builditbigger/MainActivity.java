002b725dfc8507456fadbb8222859f0c49ef83c9
Lara Martin <larikimc@gmail.com>
2017-06-04 15:58:32 +0200

    retrieve joke from javajokeprovider library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.JokeProvider;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        String joke = JokeProvider.getJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ javajokeprovider/src/main/java/com/example/JokeProvider.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class JokeProvider {
+
+    public static String getJoke() {
+        return "this is a joke";
+    }
+}
